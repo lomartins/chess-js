0be16ea0575ccfbcae17af350a902dbc0389fb1e
lucaomartins <lucasmariano.martins@outlook.com>
2020-12-05 23:47:09 -0300

    Add all pieces to board

--- chess/board.js
+++ chess/board.js
@@ -2,11 +2,34 @@ class Board {
     constructor() {
         this.whitePieces = []
         this.blackPieces = []
+        this.nullPiece = new Pawn
         this.setupPieces()
     }
 
     setupPieces() {
-        this.whitePieces.push(new King(0, 0, TEAM.BLACK))
+        this.whitePieces.push(new King(4, 0, TEAM.BLACK))
+        this.whitePieces.push(new Queen(3, 0, TEAM.BLACK))
+        this.whitePieces.push(new Rook(0, 0, TEAM.BLACK))
+        this.whitePieces.push(new Rook(7, 0, TEAM.BLACK))
+        this.whitePieces.push(new Knight(6, 0, TEAM.BLACK))
+        this.whitePieces.push(new Knight(1, 0, TEAM.BLACK))
+        this.whitePieces.push(new Bishop(2, 0, TEAM.BLACK))
+        this.whitePieces.push(new Bishop(5, 0, TEAM.BLACK))
+        for (var i = 0; i < 8; i++) {
+            this.whitePieces.push(new Pawn(i, 1, TEAM.BLACK))
+        }
+
+        this.whitePieces.push(new King(4, 7, TEAM.WHITE))
+        this.whitePieces.push(new Queen(3, 7, TEAM.WHITE))
+        this.whitePieces.push(new Rook(0, 7, TEAM.WHITE))
+        this.whitePieces.push(new Rook(7, 7, TEAM.WHITE))
+        this.whitePieces.push(new Knight(6, 7, TEAM.WHITE))
+        this.whitePieces.push(new Knight(1, 7, TEAM.WHITE))
+        this.whitePieces.push(new Bishop(2, 7, TEAM.WHITE))
+        this.whitePieces.push(new Bishop(5, 7, TEAM.WHITE))
+        for (var i = 0; i < 8; i++) {
+            this.whitePieces.push(new Pawn(i, 6, TEAM.WHITE))
+        }
     }
 
     show() {
@@ -28,14 +51,14 @@ class Board {
     getPieceAt(x, y) {
         for (var i = 0; i < this.whitePieces.length; i++) {
             if (!this.whitePieces[i].taken && this.whitePieces[i].isMatrixPositionAt(x, y)) {
-                return this.whitePieces[i];
+                return this.whitePieces[i]
             }
         }
         for (var i = 0; i < this.blackPieces.length; i++) {
             if (!this.blackPieces[i].taken && this.blackPieces[i].isMatrixPositionAt(x, y)) {
-                return this.blackPieces[i];
+                return this.blackPieces[i]
             }
         }
-        return null;
+        return nullPiece
     }
 }
--- chess/pieces.js
+++ chess/pieces.js
@@ -19,34 +19,128 @@ class Piece {
     }
 
     show() {
-        switch(this.team) {
-            case TEAM.WHITE:
-                fill(255, 255, 255)
-                break
-            case TEAM.BLACK:
-                fill(0, 0, 0)
-                break
-            default:
-                fill("#f00")
-        }
-        textSize(30)
-        textAlign(CENTER, CENTER)
-        if (this.movingThisPiece) {
-            text(this.letter, mouseX, mouseY)
-        } else {
-            text(this.letter, this.pixelPosition.x, this.pixelPosition.y)
+        if (!this.taken) {
+            imageMode(CENTER)
+            if (this.movingThisPiece) {
+                image(this.sprite, mouseX, mouseY, tileSize, tileSize)
+            } else {
+                image(this.sprite, this.pixelPosition.x, this.pixelPosition.y, tileSize * 0.85, tileSize * 0.85)
+            }
         }
     }
 
     move(x, y) {
-        this.matrixPosition = createVector(x, y);
-        this.pixelPosition = createVector(x * tileSize + tileSize / 2, y *tileSize + tileSize / 2);
+        if (board.pieceAt(x, y) && board.getPieceAt(x, y) != this) {
+            board.getPieceAt(x, y).die()
+
+        }
+        if (x < 8 && x >= 0 && y < 8 && y >= 0) {
+            this.matrixPosition = createVector(x, y);
+            this.pixelPosition = createVector(x * tileSize + tileSize / 2, y *tileSize + tileSize / 2);
+        }
+    }
+
+    die() {
+        this.taken = true
+        this.matrixPosition = createVector(-1, -1);
+        this.pixelPosition = createVector(-1, -1);
+        // deathSound.play()
     }
 }
 
 class King extends Piece {
     constructor(x, y, team) {
         super(x, y, team);
-        this.letter = 'K'
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_king"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_king"]
+                break
+            default:
+                break;
+        }
+    }
+}
+
+class Queen extends Piece {
+    constructor(x, y, team) {
+        super(x, y, team);
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_queen"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_queen"]
+                break
+            default:
+                break;
+        }
+        
+    }
+}
+
+class Rook extends Piece {
+    constructor(x, y, team) {
+        super(x, y, team);
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_rook"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_rook"]
+                break
+            default:
+                break;
+        }
+    }
+}
+
+class Bishop extends Piece {
+    constructor(x, y, team) {
+        super(x, y, team);
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_bishop"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_bishop"]
+                break
+            default:
+                break;
+        }
+    }
+}
+
+class Knight extends Piece {
+    constructor(x, y, team) {
+        super(x, y, team);
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_knight"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_knight"]
+                break
+            default:
+                break;
+        }
+    }
+}
+
+class Pawn extends Piece {
+    constructor(x, y, team) {
+        super(x, y, team);
+        switch(team) {
+            case TEAM.WHITE:
+                this.sprite = spriteMapper["white_pawn"]
+                break
+            case TEAM.BLACK:
+                this.sprite = spriteMapper["black_pawn"]
+                break
+            default:
+                break;
+        }
     }
 }
